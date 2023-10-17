b477586cfbf7f791751d58fe662103e9dc94f058
Alan <c.lyu@griffith.edu.au>
2023-10-17 16:15:49 +1000

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import plotly.express as px
 
 
 st.markdown('# Structural Analysis Project')
+st.markdown('## Project details as below:')
 
 
 st.sidebar.subheader("Parameter Inputs")
@@ -63,56 +64,77 @@ max_horizontal= appmo.find_max_horizontal(frame_model)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-tab1, tab2, tab3, tab4, tab5 = st.tabs(['Calculation','shear', 'bending','Vertical','horizontal'])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(['Calculation','Shear', 'Bending','Vertical','Horizontal'])
 with tab1:
-    st.write(f"{round(max_moment,2)} kNm")
-    st.write(f"{round(max_shear, 2)} kN")
-    st.write(f"{round(max_vertical,2)} mm")
-    st.write(f"{round(max_horizontal,2)} mm")
+    st.write(f"Max bending moment: {round(max_moment,2)} kNm")
+    st.write(f"Max shear force: {round(max_shear, 2)} kN")
+    st.write(f"Max vertical displacement: {round(max_vertical,2)} mm")
+    st.write(f"Max horizontal displacement: {round(max_horizontal,2)} mm")
 
 with tab2:
     Shear_fig = go.Figure()
-    Shear_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[0][0][0],y = appmo.get_list(frame_model)[0][0][1]))
-    Shear_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[0][1][0],y = appmo.get_list(frame_model)[0][1][1]))
-    Shear_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[0][2][0],y = appmo.get_list(frame_model)[0][2][1]))
-    Shear_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[0][3][0],y = appmo.get_list(frame_model)[0][3][1]))
+    Shear_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[0][0][0],
+                                   y = appmo.get_list(frame_model)[0][0][1],
+                                   name="Member 1",
+                                   line=dict(color = "firebrick", width = 4)))
+    Shear_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[0][1][0],
+                                   y = appmo.get_list(frame_model)[0][1][1],
+                                   name="Member 2",
+                                   line=dict(width = 4)))
+    Shear_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[0][2][0],
+                                   y = appmo.get_list(frame_model)[0][2][1],
+                                   name="Member 3",
+                                   line=dict(width = 4)))
+    Shear_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[0][3][0],
+                                   y = appmo.get_list(frame_model)[0][3][1],
+                                   name="Member 4",
+                                   line=dict(width = 4)))
     Shear_fig   
 
 with tab3:
     Moment_fig = go.Figure()
-    Moment_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[1][0][0],y = appmo.get_list(frame_model)[1][0][1]))
-    Moment_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[1][1][0],y = appmo.get_list(frame_model)[1][1][1]))
-    Moment_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[1][2][0],y = appmo.get_list(frame_model)[1][2][1]))
-    Moment_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[1][3][0],y = appmo.get_list(frame_model)[1][3][1]))
+    Moment_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[1][0][0],
+                                    y = appmo.get_list(frame_model)[1][0][1],
+                                    name="Member 1"))
+    Moment_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[1][1][0],
+                                    y = appmo.get_list(frame_model)[1][1][1],
+                                    name="Member 2"))
+    Moment_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[1][2][0],
+                                    y = appmo.get_list(frame_model)[1][2][1],
+                                    name="Member 3"))
+    Moment_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[1][3][0],
+                                    y = appmo.get_list(frame_model)[1][3][1],
+                                    name="Member 4"))
     Moment_fig
 
 with tab4:
     Vertical_dis_fig = go.Figure()
-    Vertical_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[2][0][0],y = appmo.get_list(frame_model)[2][0][1]))
-    Vertical_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[2][1][0],y = appmo.get_list(frame_model)[2][1][1]))
-    Vertical_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[2][2][0],y = appmo.get_list(frame_model)[2][2][1]))
-    Vertical_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[2][3][0],y = appmo.get_list(frame_model)[2][3][1]))
+    Vertical_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[2][0][0],
+                                          y = appmo.get_list(frame_model)[2][0][1],
+                                          name="Memnber 1"))
+    Vertical_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[2][1][0],
+                                          y = appmo.get_list(frame_model)[2][1][1],
+                                          name="Memnber 2"))
+    Vertical_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[2][2][0],
+                                          y = appmo.get_list(frame_model)[2][2][1],
+                                          name="Memnber 3"))
+    Vertical_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[2][3][0],
+                                          y = appmo.get_list(frame_model)[2][3][1],
+                                          name="Memnber 4"))
     Vertical_dis_fig
 
 with tab5:
     Horizontal_dis_fig = go.Figure()
-    Horizontal_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[3][0][0],y = appmo.get_list(frame_model)[3][0][1]))
-    Horizontal_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[3][1][0],y = appmo.get_list(frame_model)[3][1][1]))
-    Horizontal_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[3][2][0],y = appmo.get_list(frame_model)[3][2][1]))
-    Horizontal_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[3][3][0],y = appmo.get_list(frame_model)[3][3][1]))
+    Horizontal_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[3][0][0],
+                                            y = appmo.get_list(frame_model)[3][0][1],
+                                            name="Member 1"))
+    Horizontal_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[3][1][0],
+                                            y = appmo.get_list(frame_model)[3][1][1],
+                                            name="Member 2"))
+    Horizontal_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[3][2][0],
+                                            y = appmo.get_list(frame_model)[3][2][1],
+                                            name="Member 3"))
+    Horizontal_dis_fig.add_trace(go.Scatter(x = appmo.get_list(frame_model)[3][3][0],
+                                            y = appmo.get_list(frame_model)[3][3][1],
+                                            name="Member 4"))
     Horizontal_dis_fig
